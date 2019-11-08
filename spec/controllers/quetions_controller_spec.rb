a22221243d9374faef87ef539d1f74f6b5fecada
excavator <str_token@mail.ru>
2019-11-08 18:24:47 +0300

    add tests Quetions-cintr GET index

--- spec/controllers/quetions_controller_spec.rb
+++ spec/controllers/quetions_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe QuetionsController, type: :controller do
+  describe "GET #index" do
+    it 'populates an array of all quetions' do
+      quetion1=FactoryBot.create(:quetion)
+      quetion2=FactoryBot.create(:quetion)
 
+      get :index
+
+      expect(assigns(:quetions)).to match_array([quetion1, quetion2])
+    end
+
+    it 'renders index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+
+  end
 end
