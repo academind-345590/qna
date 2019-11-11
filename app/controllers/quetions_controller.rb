33ba1a3c5895de78ae65ba4a3f1fac29f62b8827
excavator <str_token@mail.ru>
2019-11-11 17:44:41 +0300

    add test and relize action xhow quetion

--- app/controllers/quetions_controller.rb
+++ app/controllers/quetions_controller.rb
@@ -4,4 +4,8 @@ class QuetionsController < ApplicationController
     @quetions=Quetion.all
   end
 
+  def show
+    @quetion=Quetion.find(params[:id])
+  end
+
 end
--- spec/controllers/quetions_controller_spec.rb
+++ spec/controllers/quetions_controller_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe QuetionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+  describe "GET #show" do
+    let(:quetion) {create(:quetion)}
+    it 'assigns the requested quetion to @quetion' do
+      get :show, params: {id: quetion}
+      expect(assigns(:quetion)).to eq quetion
+    end
+    it 'render show view' do
+      get :show, params: {id: quetion}
+      expect(response).to render_template :show
+    end
+  end
 end
