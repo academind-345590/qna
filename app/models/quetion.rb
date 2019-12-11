fb4bf0085710c83daeac2a04c3b0bcc0d080abb3
excavator <str_token@mail.ru>
2019-12-11 14:11:22 +0300

    add nested attributes to model

--- app/models/quetion.rb
+++ app/models/quetion.rb
@@ -1,5 +1,8 @@
 class Quetion < ApplicationRecord
   validates :title, :body, presence: true
+
   has_many :answers
   has_many :attachments
+
+  accepts_nested_attributes_for :attachments
 end
--- spec/models/quetion_spec.rb
+++ spec/models/quetion_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Quetion, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
 
+  it { should accept_nested_attributes_for :attachments}
+
 end
